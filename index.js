13a30a545ec70e7d2cf3e7e30698c03246ee9620
Rohit Kumar <rohitkumar.devexpert@gmail.com>
2025-11-23 17:42:15 +0530

    changing cors route

--- index.js
+++ index.js
@@ -24,7 +24,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"http://localhost:3000",
+
+        origin:[
+             "http://localhost:3000",
+             "https://studynotion-frontend-git-76c322-rohit-kumars-projects-cdee1d77.vercel.app/"
+        ],
         credentials:true,
     })
 )
